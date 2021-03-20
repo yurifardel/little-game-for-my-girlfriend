51f0cb6c9de8d15fa2bf602574112e50e747d72e
yurifardel <yrnascimento@outlook.com>
2021-03-20 14:32:24 -0300

    att.

--- src/pages/SelectMenu.tsx
+++ src/pages/SelectMenu.tsx
@@ -1,27 +1,40 @@
 import styles from '../styles/pages/SelectMenu.module.css';
+import Head from 'next/head';
 
 export default function SelectMenu() {
   return (
+    <>
+    <Head>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet"/>
+    </Head>
     <div className={styles.SelectMenuContainer}>
       <div className={styles.SelectMenuContent}>
-        <h1>Escolha seu personagem.</h1>
+        <h1 style={{color: 'black'}} >Escolha seu personagem.</h1>
 
         <div className={styles.SelectMenuOptions}>
-          <div>
+          <div className={styles.Container} id={styles.Yuri}>
             <a href="">
-              <img src="image/yuri.png" alt="yuri" />
+              <h1>Yuri</h1>
             </a>
-            <h1>yuri</h1>
           </div>
 
-          <div>
+          <div className={styles.Container} id={styles.Pilar}>
             <a href="">
-              <img src="image/amor.png" alt="amor" />
+              <h1>Pilar</h1>
             </a>
-            <h1>Pilar</h1>
           </div>
         </div>
       </div>
     </div>
+
+      <div className={styles.Footer}>
+        <footer>
+          <div>
+            <a href="">Sem conteudo</a>
+          </div>
+        </footer>
+      </div>
+    </>
   );
 }
